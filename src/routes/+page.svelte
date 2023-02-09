be358108693f318f344a056c86675a56fda076ca
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-02-09 14:10:30 +0100

    Font dev

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<h1>Index</h1>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,500&display=swap');
+
+<div class="flex flex-row justify-center"> 
+    <div class="bg-blue-200 h-32 w-6/12 p-2">
+        <h1 class="text-3xl font-extrabold font">The friendliest</h1>
+        <h1 class="text-3xl font-extrabold">passwordmanager</h1>
+    </div>
+    <div class="bg-blue-200 h-32 w-6/12 p-2">
+        <h1>Fuck dig</h1>
+    </div>
+</div>

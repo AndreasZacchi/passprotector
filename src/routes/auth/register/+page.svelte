17c28f713dab81968c07125bf40a55dc29981c1f
Rondywastaken <67154928+Rondywastaken@users.noreply.github.com>
2023-02-10 00:15:14 +0100

    Added background

--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -3,19 +3,21 @@
 </script>
 
 <!-- Window div -->
-<div class="flex justify-center h-[calc(100vh-4rem)] items-center">
+<div class="flex justify-center h-[calc(100vh-4rem)] items-center" 
+style="background-color: rgb(249, 115, 22); background-image: radial-gradient(at 0% 100%, rgb(220, 38, 38) 0, transparent 100%), radial-gradient(at 0% 0%, rgb(249, 115, 22) 0, transparent 100%), radial-gradient(at 100% 100%, rgb(253, 224, 71) 0, transparent 100%), radial-gradient(at 100% 0%, rgb(251, 146, 60) 0, transparent 100%);
+">
 
 	<!-- Register form -->
-	<form action="?/register" method="POST" class="bg-main-200 rounded-3xl py-2 w-fit p-6 pt-3 pb-4 p-10 text-xl">
+	<form action="?/register" method="POST" class="bg-white rounded-3xl py-2 w-fit p-6 pt-3 pb-4 p-10 text-3xl">
 
 		<!-- Logo div -->
 		<div class="flex justify-center">
-			<img src="/logo.png" alt="logo" class="object-cover h-21 w-20">
+			<img src="/logo.png" alt="logo" class="object-cover w-20">
 		</div>
 
 		<!-- Register text div -->
 		<div class="flex justify-center">
-			<h1 class="text-4xl pb-4 text-white">Register</h1>
+			<h1 class="text-5xl pb-4">Register</h1>
 		</div>
 
 		<!-- Inputs -->

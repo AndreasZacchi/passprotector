e162b78b2232ba17de7b824d1d36e401912a5382
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-02-10 13:02:57 +0100

    Mainpage design 60% done
    +Reviews
    +Trustpilot rating
    +Placeholder picture
    +Ad text
    +Button to register
    
    missing
    -button needs to get linked to register
    -Reviews needs redesign
    -missing more 5 star reviews

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,35 @@
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,500&display=swap');
+<div style="background-color: white;"
+class="grid grid-cols-2 h-[calc(100vh-2rem)]"> 
 
-<div class="flex flex-row justify-center"> 
-    <div class="bg-blue-200 h-32 w-6/12 p-2">
-        <h1 class="text-3xl font-extrabold font">The friendliest</h1>
-        <h1 class="text-3xl font-extrabold">passwordmanager</h1>
+<!--Ad text and button ment to lure people in-->
+    <div class="flex flex-col w-12/12 h-3/3 p-8">
+        <h1 class="text-4xl font-poppins font-bold text-main-200">
+            Take control of your online security with ease!
+        </h1>
+        <span class="mt-2">
+            Unlock the power of online security with PassProtector! Say goodbye to endless password resets, account lockouts, and security headaches. Store all your passwords in one secure and convenient place and enjoy peace of mind with just one master password. Make PassProtector your digital bodyguard and experience hassle-free, rock-solid security. Get started today!
+        </span>
+        <button type="button" 
+        class="mt-11 bg-main-300 rounded-md h-10 w-60 font-poppins text-white ease-in duration-150 hover:bg-main-200">
+            Try PassProtector for Free
+        </button>
     </div>
-    <div class="bg-blue-200 h-32 w-6/12 p-2">
-        <h1>Fuck dig</h1>
+
+<!--Picture example div, ment to show example of our website when logged in-->
+    <div class="w-12/12 p-8">
+        <img src="PLACEHOLDER.png" alt="PassProtector example">
+    </div>
+
+<!--Reviews-->
+    <div class="bg-main-300 text-white p-5">
+        <h1 class="italic font-bold mb-1">Marques Brownley (MKBHD)</h1>
+        <span>
+            If you're looking for a new password manager, I'd definitely give PassProtector a look.
+        </span>
+    </div>
+
+<!--stars-->
+    <div class="bg-main-300 flex items-center">
+        <img class="w-48 h-16 mr-4 object-center" src="trustpilot5stars.png" alt="Trustpilot rating">
     </div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,17 @@ module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				poppins: ['poppins']
+			},
 			colors: {
 				main: {
-					100: '#f1a06a',
-					200: '#ea6b17',
-					300: '#bb5511'
-				}
+                    100: '#f1a06a',
+                    200: '#FF8833',
+                    300: '#ff6b00',
+                    400: '#F56600',
+                    500: '#E05E00'
+                }
 			}
 		}
 	},

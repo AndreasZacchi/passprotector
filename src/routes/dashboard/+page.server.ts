6e55d329ef638fcfdcb33e4f5317bd1c088fe920
AndreasZacchi <zacchiandreas@gmail.com>
2023-02-12 23:54:00 +0100

    Ability to copy password (Secure over HTTPS)

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -3,9 +3,24 @@ import { error, redirect, type Actions, type ServerLoad } from '@sveltejs/kit';
 
 import { SECRET } from '$env/static/private';
 
-export const load: ServerLoad = ({ locals }) => {
+export const load: ServerLoad = async ({ locals }) => {
 	if (!locals.pb.authStore.isValid) {
 		throw redirect(302, '/auth/login');
+	}
+
+	const records = await locals.pb.collection('passwords').getFullList(200, {
+		sort: '-website',
+		filter: `user.id="${locals.user.id}"`
+	});
+
+	if (records[0] !== undefined) {
+		let passwords = new Array();
+		records.forEach((e) => {
+			passwords.push([e.website, getPassword(e.password, SECRET)]);
+		});
+		return {
+			passwords
+		};
 	}
 };
 
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Input from '$lib/components/Input.svelte';
+	import { getPassword } from '$lib/utils';
+	import { Record } from 'pocketbase';
 
-	export let data: { user: Record };
+	export let data: { user: Record; passwords: [website: string, password: string] | undefined };
 </script>
 
 <form action="?/generatePassword" method="POST" class="bg-gray-500 py-2">
@@ -11,6 +13,25 @@
 	</div>
 </form>
 
-<ul>
-	<li />
-</ul>
+{#if data.passwords !== undefined}
+	<table>
+		<thead>
+			<tr>
+				<th>Website</th>
+				<th>Password</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.passwords as [website, password]}
+				<tr>
+					<td>{website}</td>
+					<td>
+						<button on:click={() => navigator.clipboard.writeText(password)}>Copy password</button>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+{:else}
+	<p>You dont have any passwords. Generate one to get started!</p>
+{/if}

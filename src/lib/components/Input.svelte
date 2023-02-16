ff37f96cacc87d46bbb612622dd00937328402c2
Rondywastaken <67154928+Rondywastaken@users.noreply.github.com>
2023-02-16 01:09:09 +0100

    Updating on register page. Getting final design ideas.

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -3,17 +3,24 @@
 	export let placeholder = '';
 	export let id: string;
 	export let type = 'text';
+	export let label: string;
 	export let disabled = false;
 	export let required = false;
 	export let error = "Something went wrong";
 </script>
 
-<div class="mt-2">
-	<input class="border-main-200 border-2 rounded-lg p-1 text-2xl"
-	 {type} {placeholder} {required} {disabled} {id} name={id} {value} />
-	<div>
+<div class="mt-2 flex flex-col">
+
+	<label class="" for="{id}">
+		<span class=" text-2xl">{label}</span>
+	</label>
+
+	<input class="border-b-2 border-black outline-none p-1 text-lg w-80" 
+	{type} {placeholder} {required} {disabled} {id} name={id} {value} />
+
+	<div class="mb-2">
 		{#if error}
-		<span class="text-sm border-red">{error}</span>
+		<span class="text-sm text-red-500">{error}</span>
 		{/if}
 	</div>
 	
--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -6,26 +6,26 @@
 <div class="flex justify-center h-[calc(100vh-4rem)] items-center">
 
 	<!-- Register form -->
-	<form action="?/register" method="POST" class="bg-white rounded-3xl py-2 w-fit pt-3 pb-4 p-10 text-3xl border-main-300 border-4">
+	<form action="?/register" method="POST" class=" shadow-2xl rounded-3xl py-2 w-fit p-20 pt-10 pb-10 text-3xl ">
 
 		<!-- Logo div -->
-		<div class="flex justify-center">
-			<img src="/logo.png" alt="logo" class="object-cover w-20">
+		<div class="flex justify-center mb-4 h-24">
+			<img src="/logo.png" alt="logo" class="object-cover w-20 ">
 		</div>
 
 		<!-- Register text div -->
 		<div class="flex justify-center">
-			<h1 class="text-5xl pb-4">Register</h1>
+			<h1 class="text-4xl pb-4">Register</h1>
 		</div>
 
 		<!-- Inputs -->
-		<Input type="email" placeholder="Email" id="email" />
-		<Input type="password" placeholder="Password" id="password" />
-		<Input type="password" placeholder="Confirm Password" id="passwordConfirm" />
+		<Input type="email" label="Email"  id="email" />
+		<Input type="password" label="Password"  id="password" />
+		<Input type="password" label="Confirm Password"  id="passwordConfirm" />
 
 		<!-- Button div -->
 		<div class="mt-2 flex justify-center pt-3">
-			<button type="submit" class="bg-main-200 rounded-full p-3 pt-2">Register</button>
+			<button type="submit" class=" bg-main-300  rounded-2xl w-8/12 p-3 pt-2 m-2 hover:bg-main-200">Register</button>
 		</div>
 
 	</form>

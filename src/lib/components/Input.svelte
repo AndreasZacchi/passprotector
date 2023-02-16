dbed96668bc9eaeb9cac31b1190edad078919479
Rondywastaken <itzrondy@gmail.com>
2023-02-16 15:25:25 +0100

    login page

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -15,7 +15,8 @@
 		<span class=" text-2xl">{label}</span>
 	</label>
 
-	<input class="border-b-2 border-black outline-none p-1 text-lg w-80 focus:border-main-300 focus:border-b-3 transition-colors delay-75 hover:border-gray-700 hover:border-b-3" 
+	<input class="border-b-2 border-black outline-none text-lg w-80
+	focus:border-main-300 transition-colors delay-75 hover:border-gray-700 hover:border-b-3 hover:mb-[-0.4px]" 
 	{type} {placeholder} {required} {disabled} {id} name={id} {value} />
 
 	<div class="mb-2">
--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,11 +1,33 @@
-<script>
-	import Input from '../../../lib/components/Input.svelte';
+<script lang="ts">
+	import Input from '$lib/components/Input.svelte';
 </script>
 
-<form action="?/login" method="POST" class="bg-gray-500 py-2">
-	<Input type="email" id="email" label="Email" />
-	<Input type="password" id="password" label="password" />
-	<div class="mt-2">
-		<button type="submit">Login</button>
-	</div>
-</form>
+<!-- Window div -->
+<div class="flex justify-center h-[calc(100vh-4rem)] items-center">
+
+	<!-- Register form -->
+	<form action="?/register" method="POST" class=" shadow-2xl rounded-3xl py-2 w-fit p-16 pt-5 pb-5 text-3xl ">
+
+		<!-- Logo div -->
+		<div class="flex justify-center h-24 ">
+			<img src="/logo.png" alt="logo" class="object-cover w-20 pb-1">
+		</div>
+
+		<!-- Register text div -->
+		<div class="flex justify-center">
+			<h1 class=" text-4xl pb-1">Login</h1>
+		</div>
+
+		<!-- Inputs -->
+		<Input type="email" label="Email"  id="email" />
+		<Input type="password" label="Password"  id="password" />
+
+		<!-- Button div -->
+		<div class="mt-2 flex justify-center pt-3">
+			<button type="submit" class=" bg-main-300 rounded-2xl w-10/12 p-3 pt-2 m-2 hover:opacity-90">Login</button>
+		</div>
+
+	</form>
+
+</div>
+

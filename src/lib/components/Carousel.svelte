e03718fbacf40c4d410719bd98d99ee7f6a908e1
Rondywastaken <67154928+Rondywastaken@users.noreply.github.com>
2023-02-23 02:14:10 +0100

    start

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    type image = {
+        name: string
+        src: string
+        review: string
+    }
+    
+    export let images: image[];
+
+</script>
+
+<ul>
+    {#each images as { name, src, review }}
+    <li>
+        <img {name} {src} {review} alt="hello" />
+    </li>
+    {/each}
+    
+</ul>

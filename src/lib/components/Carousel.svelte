dd742aeabe725ba2f606c5b21b7f9b0ad5060a52
Rondywastaken <itzrondy@gmail.com>
2023-03-06 18:23:14 +0100

    dfghjklæø

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+	import { transition_out } from "svelte/internal";
+  import { slide } from "svelte/transition";
+
+
     let images = [
       {
         name: "Jeff Bezos",
@@ -18,16 +22,11 @@
     ];
   
     let currentIndex = 1;
-    //const card = document.getElementById("card")
   
     function next() {
       currentIndex = (currentIndex + 1) % images.length;
-     
-      /* if (card) {
-        card.style.animationName = "nextCard"
-        card.style.animationDuration = "2s"
-      } */
-      
+      const card = document.getElementsByClassName("carousel-card")
+      card.set.active
     }
   
     function previous() {
@@ -37,19 +36,15 @@
   </script>
   
   <style>
-    @keyframes nextCard {
-        from { transform: translateX(5rem) }
-    }
 
     .carousel-card {
       opacity: 0.5;
-      animation-name: nextCard;
-      animation-duration: 2s;
     }
   
     .carousel-card.active {
       opacity: 1;
-      
+      transform: translateX(-10rem);
+      transition: transform 2s linear;
     }
 
     
@@ -60,7 +55,7 @@
   
     {#each images as image, i}
       <div id="card"
-        class={` carousel-card flex flex-col h-[22rem] w-[25rem] rounded-2xl shadow-xl ${i == currentIndex ? 'active' : ''}`}
+        class={` carousel-card flex flex-col h-[22rem] w-[25rem] rounded-2xl shadow-xl`}
       >
         <div class=" bg-main-300 flex justify-center h-[40%] rounded-t-2xl">
           <img class="mt-2 border-[6px] border-main-300 w-48 h-48 rounded-[50%] object-cover" src={image.src} alt="hello" />

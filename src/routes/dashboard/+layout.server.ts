1ccce56d48f1840409d842329f36715d83c5669d
AndreasZacchi <42937668+AndreasZacchi@users.noreply.github.com>
2023-03-15 11:33:26 +0100

    Removed more unneccesary code

--- src/routes/dashboard/+layout.server.ts
+++ src/routes/dashboard/+layout.server.ts
@@ -1,7 +1,24 @@
 import { redirect, type ServerLoad } from '@sveltejs/kit';
+import { getPassword } from '$lib/utils';
+import { SECRET } from '$env/static/private';
 
 export const load: ServerLoad = async ({ locals }) => {
 	if (!locals.pb.authStore.isValid) {
 		throw redirect(302, '/auth/login');
+	}
+
+	const records = await locals.pb.collection('passwords').getFullList(200, {
+		sort: '-website',
+		filter: `user.id="${locals.user.id}"`
+	});
+
+	if (records[0] !== undefined) {
+		let passwords = new Array();
+		records.forEach((e) => {
+			passwords.push([e.id, e.website, getPassword(e.password, SECRET)]);
+		});
+		return {
+			passwords
+		};
 	}
 };
--- src/routes/dashboard/passwords/+page.server.ts
+++ src/routes/dashboard/passwords/+page.server.ts
@@ -1,24 +1,7 @@
-import { encryptUserPassword, generatePassword, getPassword } from '$lib/utils';
-import { error, redirect, type Actions, type ServerLoad } from '@sveltejs/kit';
+import { encryptUserPassword, generatePassword } from '$lib/utils';
+import { error, redirect, type Actions } from '@sveltejs/kit';
 
 import { SECRET } from '$env/static/private';
-
-export const load: ServerLoad = async ({ locals }) => {
-	const records = await locals.pb.collection('passwords').getFullList(200, {
-		sort: '-website',
-		filter: `user.id="${locals.user.id}"`
-	});
-
-	if (records[0] !== undefined) {
-		let passwords = new Array();
-		records.forEach((e) => {
-			passwords.push([e.id, e.website, getPassword(e.password, SECRET)]);
-		});
-		return {
-			passwords
-		};
-	}
-};
 
 export const actions: Actions = {
 	generatePassword: async ({ locals, request }) => {

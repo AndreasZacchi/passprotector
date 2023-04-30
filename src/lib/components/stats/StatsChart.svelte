61b571ce864e0096eb964cfe32abb78c13f22e89
MattyMcF4tty <matthias.r.kristensen@gmail.com>
2023-04-30 21:08:05 +0200

    start dev change password to passwordcount (Not working)

--- src/lib/components/stats/StatsChart.svelte
+++ src/lib/components/stats/StatsChart.svelte
@@ -8,13 +8,17 @@
 	class="flex flex-col bg-white rounded-xl p-4 items-center justify-center shadow-lg h-full w-full"
 >
 	<!-- Chart menu -->
-	<div class="flex flex-row">
+	<div class="flex flex-row w-full items-center justify-center">
 		<h1 class="text-2xl">{shownData} Over Time</h1>
-		<select id="graphData" class="">
+		<select id="graphData" class="justify-self-end place-self-end">
 			<option value="ranked">Ranked Passwords</option>
 			<option value="score">Password Score</option>
 			<option value="leakRisk">Password Leak Risk</option>
 		</select>
 	</div>
 	<!-- installer chart.js -->
+	<!-- Midlertidig -->
+	<div class="flex border-2 border-slate-200 h-full w-full items-center justify-center">
+		<p>Chart</p>
+	</div>
 </div>
